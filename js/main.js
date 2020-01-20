f9dc0c6c40d51fa46253a1294cf38b341425be55
Gennady <thegoodlife4@mail.ru>
2020-01-20 18:27:52 +0300

    Изменил js дописал слайдер секции партнеров

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // функции handleShadowVisibility & toggleShadow отвечают за
 // появление тени в reviewsMenu, для прокручивания
-function handleShadowVisibility() {
-    const maxScrollStartReached = reviewsMenu.scrollLeft <= 0;
-    const maxScrollEndReached = reviewsMenu.scrollLeft >= reviewsMenu.scrollWidth - reviewsMenu.offsetWidth;
-    toggleShadow(shadowStart, maxScrollStartReached);
-    toggleShadow(shadowEnd, maxScrollEndReached);
+function handleShadowVisibility(fatherElem, elShadowStart, elShadowEnd) {
+    const maxScrollStartReached = fatherElem.scrollLeft <= 0;
+    const maxScrollEndReached = fatherElem.scrollLeft >= fatherElem.scrollWidth - fatherElem.offsetWidth;
+    toggleShadow(elShadowStart, maxScrollStartReached);
+    toggleShadow(elShadowEnd, maxScrollEndReached);
 }
 function toggleShadow(el, maxScrollReached) {
     const shadowIsVisible = el.classList.contains('is-visible');
@@ -17,6 +17,7 @@ function toggleShadow(el, maxScrollReached) {
         window.requestAnimationFrame(() => el.classList.remove('is-visible'));
     }
 }
+
 // функция отвечает за переключение пунктов в reviewsMenu
 function reviewsContentViev(el) {
     if(!el.target.classList.contains('active')) {
@@ -35,8 +36,6 @@ function reviewsContentViev(el) {
                         allReviewsContentBlock[4].classList.add('hidden-content');
                     case "Все":
                         allReviewsContentBlock[5].classList.add('hidden-content');
-                        // generationBigSlider(5);
-                        break;
                     default:
                         break;
                 }
@@ -51,19 +50,19 @@ function reviewsContentViev(el) {
                 contentItem = allReviewsContentBlock[reviewsContentBlockIndex].children;
                 // функция для работы с экранами на которых будут стрелки навигации
                 // находим какие элементы скрыть, какие показать
-                findVisibleItem();
+                findVisibleItem(contentItem, "content");
                 // проверяем нужно ли показывать стрелки после переключения блока
-                showVisibleArrow();
+                showVisibleArrow(visibleItem.firstHidden, visibleItem.firstVisible, contentItem, contentArrowRight, contentArrowLeft);
             }
         }
         // вешаем на нужный пункт reviewsMenu класс active
         el.target.classList.add('active');
         // перемещаем скролл на данную позицию в меню
-        reviewsMenu.scrollLeft = el.target.offsetLeft
+        // reviewsMenu.scrollLeft = el.target.offsetLeft
         // убираем класс hidden-content с блока, который должен появиться
         allReviewsContentBlock[reviewsContentBlockIndex].classList.remove("hidden-content");
         // обновляем состояния скролла после отображения элементов
-        handleShadowVisibilityContent();
+        handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd);
     }
 }
 // Функция возвращает исходное состояние предыдущего блока слайдера
@@ -82,152 +81,94 @@ function returnState() {
             contentItem[i].classList.remove("hidden");
     }
 }
-// функции отвечающие за скролл видео в контетн слайдере
-function handleShadowVisibilityContent() {
-    var maxScrollStartReached = allReviewsContentBlock[reviewsContentBlockIndex].scrollLeft <= 0;
-    var maxScrollEndReached = allReviewsContentBlock[reviewsContentBlockIndex].scrollLeft >= allReviewsContentBlock[reviewsContentBlockIndex].scrollWidth - allReviewsContentBlock[reviewsContentBlockIndex].offsetWidth;
-    toggleShadowContent(contentShadowStart, maxScrollStartReached );
-    toggleShadowContent(contentShadowEnd, maxScrollEndReached);
-}
-function toggleShadowContent(el, maxScrollReached) {
-    var shadowIsVisible = el.classList.contains('is-active');
-    var showShadow = !maxScrollReached && !shadowIsVisible;
-    var hideShadow = maxScrollReached && shadowIsVisible;
-    // requestAnimationFrame для оптимального исполнения прокрутки
-    if(showShadow) {
-        window.requestAnimationFrame(() => el.classList.add('is-active'));        
-    } else if(hideShadow) {
-        window.requestAnimationFrame(() => el.classList.remove('is-active'));
-    }
-}
 // функция появления слайдов от взаимодействия с стрелками
 function contentSliderMove(el) {
     if(el.target == contentArrowRight) {
         if(contentArrowRight.style.cursos != "default" && contentArrowRight.style.opacity != "0") {
-            findVisibleItem();
-            contentItem[firstVisible].classList.add('hidden');
-            contentItem[firstHidden].classList.remove('hidden');
-            firstHidden++; firstVisible++;
+            findVisibleItem(contentItem, "content");
+            contentItem[visibleItem.firstVisible].classList.add('hidden');
+            contentItem[visibleItem.firstHidden].classList.remove('hidden');
+            visibleItem.firstHidden++; visibleItem.firstVisible++;
         }
     }
     if(el.target == contentArrowLeft) {
         if(contentArrowLeft.style.cursos != "default" && contentArrowLeft.style.opacity != "0") {
-            contentItem[firstHidden - 1].classList.add("hidden");
-            contentItem[firstVisible - 1].classList.remove("hidden");
-            firstHidden--; firstVisible--;
+            contentItem[visibleItem.firstHidden - 1].classList.add("hidden");
+            contentItem[visibleItem.firstVisible - 1].classList.remove("hidden");
+            visibleItem.firstHidden--; visibleItem.firstVisible--;
         }
     }
-    showVisibleArrow();
+    showVisibleArrow(visibleItem.firstHidden, visibleItem.firstVisible, contentItem, contentArrowRight, contentArrowLeft);
 }
 // функця отвечает за показывать ли нужную стрелку или нет
-function showVisibleArrow() {
+function showVisibleArrow(firstHiddenEl, firstVisibleEl, itemLenght, arrowRight, arrowLeft) {
     // Показывать ли правую стрелку
-    if(firstHidden == contentItem.length) {
-        contentArrowRight.style.opacity = 0;
-        contentArrowRight.style.cursor = "default";
+    if(firstHiddenEl == itemLenght.length) {
+        arrowRight.style.opacity = 0;
+        arrowRight.style.cursor = "default";
     } else {
-        contentArrowRight.style.opacity = 1;
-        contentArrowRight.style.cursor = "pointer";
+        arrowRight.style.opacity = 1;
+        arrowRight.style.cursor = "pointer";
     }
     // показывать ли левую стрелку
-    if(firstVisible != 0) {
-        contentArrowLeft.style.opacity = 1;
-        contentArrowLeft.style.cursor = "pointer";
+    if(firstVisibleEl != 0) {
+        arrowLeft.style.opacity = 1;
+        arrowLeft.style.cursor = "pointer";
     } else {
-        contentArrowLeft.style.opacity = 0;
-        contentArrowLeft.style.cursor = "default";
+        arrowLeft.style.opacity = 0;
+        arrowLeft.style.cursor = "default";
     }
 }
 // функция которая определяет сколько в блоке показывать видимых элементов
-function findVisibleItem() {
-    for(let i = 0; i < contentItem.length; i++) {
-        if(!contentItem[i].classList.contains("hidden")) {
-            firstVisible = i;
-            break;
+function findVisibleItem(itemLenght, elementName) {
+    if(elementName == 'content') {
+        for(let i = 0; i < itemLenght.length; i++) {
+            if(!itemLenght[i].classList.contains("hidden")) {
+                visibleItem.firstVisible = i;
+                break;
+            }
         }
-    }
-    for(let i = contentItem.length-1; i >= 0; i--) {
-        if(!contentItem[i].classList.contains("hidden"))
-            break;
-        else
-            firstHidden = i;
-    }
+        for(let i = itemLenght.length-1; i >= 0; i--) {
+            if(!itemLenght[i].classList.contains("hidden"))
+                break;
+            else
+                visibleItem.firstHidden = i;
+        }
+   }
+   else if(elementName == 'partner') {
+    for(let i = 0; i < itemLenght.length; i++) {
+            if(!itemLenght[i].classList.contains("hidden")) {
+                visibleItem.firstVisiblePartner = i;
+                break;
+            }
+        }
+        for(let i = itemLenght.length-1; i >= 0; i--) {
+            if(!itemLenght[i].classList.contains("hidden"))
+                break;
+            else
+                visibleItem.firstHiddenPartner = i;
+        }
+   }
 }
-// функция показа тени для партнерского слайдера
-function handleShadowVisibilityPartner() {
-    const maxScrollStartReached = partnerSlider.scrollLeft <= 0;
-    const maxScrollEndReached = partnerSlider.scrollLeft >= partnerSlider.scrollWidth - partnerSlider.offsetWidth;
-    toggleShadow(shadowPartnerStart, maxScrollStartReached);
-    toggleShadow(shadowParnerEnd, maxScrollEndReached);
-}
-function toggleShadow(el, maxScrollReached) {
-    const shadowIsVisible = el.classList.contains('is-visible');
-    const showShadow = !maxScrollReached && !shadowIsVisible;
-    const hideShadow = maxScrollReached && shadowIsVisible;
-    // requestAnimationFrame для оптимального исполнения прокрутки
-    if(showShadow) {
-        window.requestAnimationFrame(() => el.classList.add('is-visible'));        
-    } else if(hideShadow) {
-        window.requestAnimationFrame(() => el.classList.remove('is-visible'));
-    }
-}
-
 // работа слайдера партнеров со стрелками
 function partnerSliderMove(el) {
     if(el.target == partnerArrowRight) {
         if(partnerArrowRight.style.cursos != "default" && partnerArrowRight.style.opacity != "0") {
-            findVisibleItemPartner();
-            partnerSliderItem[firstVisiblePartner].classList.add('hidden');
-            partnerSliderItem[firstHiddenPartner].classList.remove('hidden');
-            firstHiddenPartner++; firstVisiblePartner++;
+            findVisibleItem(partnerSliderItem, 'partner');
+            partnerSliderItem[visibleItem.firstVisiblePartner].classList.add('hidden');
+            partnerSliderItem[visibleItem.firstHiddenPartner].classList.remove('hidden');
+            visibleItem.firstHiddenPartner++; visibleItem.firstVisiblePartner++;
         }
     }
     if(el.target == partnerArrowLeft) {
         if(partnerArrowLeft.style.cursos != "default" && partnerArrowLeft.style.opacity != "0") {
-            partnerSliderItem[firstHiddenPartner - 1].classList.add("hidden");
-            partnerSliderItem[firstVisiblePartner - 1].classList.remove("hidden");
-            firstHiddenPartner--; firstVisiblePartner--;
+            partnerSliderItem[visibleItem.firstHiddenPartner - 1].classList.add("hidden");
+            partnerSliderItem[visibleItem.firstVisiblePartner - 1].classList.remove("hidden");
+            visibleItem.firstHiddenPartner--; visibleItem.firstVisiblePartner--;
         }
     }
-    showVisibleArrowPartner();
+    showVisibleArrow(visibleItem.firstHiddenPartner, visibleItem.firstVisiblePartner, partnerSliderItem, partnerArrowRight, partnerArrowLeft);
 }
-// функця отвечает за показывать ли нужную стрелку или нет партнеров
-function showVisibleArrowPartner() {
-    // Показывать ли правую стрелку
-    if(firstHiddenPartner == partnerSliderItem.length) {
-        partnerArrowRight.style.opacity = 0;
-        partnerArrowRight.style.cursor = "default";
-    } else {
-        partnerArrowRight.style.opacity = 1;
-        partnerArrowRight.style.cursor = "pointer";
-    }
-    // показывать ли левую стрелку
-    if(firstVisiblePartner != 0) {
-        partnerArrowLeft.style.opacity = 1;
-        partnerArrowLeft.style.cursor = "pointer";
-    } else {
-        partnerArrowLeft.style.opacity = 0;
-        partnerArrowLeft.style.cursor = "default";
-    }
-}
-// функция которая определяет сколько в блоке показывать видимых элементов
-// в слайдере партнеров
-function findVisibleItemPartner() {
-    for(let i = 0; i < partnerSliderItem.length; i++) {
-        if(!partnerSliderItem[i].classList.contains("hidden")) {
-            firstVisiblePartner = i;
-            break;
-        }
-    }
-    for(let i = partnerSliderItem.length-1; i >= 0; i--) {
-        if(!partnerSliderItem[i].classList.contains("hidden"))
-            break;
-        else
-            firstHiddenPartner = i;
-    }
-}
-
-
 /*--------------ПЕРЕМЕННЫЕ ---------------- */
 // Тут у нас будут функции для работы с навигацией в "отзывах"
 
@@ -249,35 +190,43 @@ var contentArrowRight = document.querySelector('.reviews__arrow--right');
 var maxVisibleSlide = 2;
 var contentItem = allReviewsContentBlock[reviewsContentBlockIndex].children;
 // переменные хранящие индекс видимых и показанных слайдов для обработки стрелки
-var firstVisible;
-var firstHidden;
+// var firstVisible;
+// var firstHidden;
+let visibleItem = {
+    firstHidden: 0,
+    firstVisible: 0,
+    firstVisiblePartner: 0,
+    firstHiddenPartner: 0
+};
+findVisibleItem(contentItem, visibleItem.firstVisible, visibleItem.firstHidden);
+
 // переменные для слайдера партнеров
 var partnerSlider = document.querySelector('.slider__box');
 var shadowPartnerStart = document.querySelector('.partners__shadow--start');
-var shadowParnerEnd = document.querySelector('.partners__shadow--end');
+var shadowPartnerEnd = document.querySelector('.partners__shadow--end');
 // Переменные для работы со стрелками слайдера партнеров
-var firstHiddenPartner;
-var firstVisiblePartner;
+// var firstHiddenPartner;
+// var firstVisiblePartner;
 var partnerSliderItem = document.querySelectorAll('.slider__item');
 var partnerArrowLeft = document.querySelector('.slider__arrow--left');
 var partnerArrowRight = document.querySelector('.slider__arrow--right');
 
 
 // вызываем для обновления состояния 
-handleShadowVisibility();
+handleShadowVisibility(reviewsMenu, shadowStart, shadowEnd);
 // вешаем слушатель на скролл reviewsMenu
-reviewsMenu.addEventListener('scroll', (e) => handleShadowVisibility(e));
+reviewsMenu.addEventListener('scroll', (e) => handleShadowVisibility(reviewsMenu, shadowStart, shadowEnd));
 // вешаем слушатель для обработки тапов по кнопкам в слайд-меню
 reviewsMenu.addEventListener('click', (e) => reviewsContentViev(e) );
 // обновление состояния контент блока
-handleShadowVisibilityContent();
+handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd);
 // вешаем слушатель на каждый контент блок 
-allReviewsContentBlock[0].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
-allReviewsContentBlock[1].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
-allReviewsContentBlock[2].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
-allReviewsContentBlock[3].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
-allReviewsContentBlock[4].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
-allReviewsContentBlock[5].addEventListener('scroll', (e) => handleShadowVisibilityContent(e));
+allReviewsContentBlock[0].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
+allReviewsContentBlock[1].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
+allReviewsContentBlock[2].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
+allReviewsContentBlock[3].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
+allReviewsContentBlock[4].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
+allReviewsContentBlock[5].addEventListener('scroll', (e) => handleShadowVisibility(allReviewsContentBlock[reviewsContentBlockIndex], contentShadowStart, contentShadowEnd));
 // скрываем изначально левую стрелку
 contentArrowLeft.style.opacity = 0;
 contentArrowLeft.style.cursor = "default";
@@ -288,9 +237,9 @@ partnerArrowLeft.style.cursor = "default";
 contentArrowRight.addEventListener('click', (event) => contentSliderMove(event));
 contentArrowLeft.addEventListener('click', (event) => contentSliderMove(event));
 // вызываем для объявления состояния
-handleShadowVisibilityPartner();
+handleShadowVisibility(partnerSlider, shadowPartnerStart, shadowPartnerEnd);
 // добавляем слушатель на скролл слайдера партнеров
-partnerSlider.addEventListener('scroll', (e) => handleShadowVisibilityPartner(e));
+partnerSlider.addEventListener('scroll', (e) => handleShadowVisibility(partnerSlider, shadowPartnerStart, shadowPartnerEnd));
 // Добавляем слушателей на стрелки слайдера партнеров
 partnerArrowRight.addEventListener('click', (event) => partnerSliderMove(event));
 partnerArrowLeft.addEventListener('click', (event) => partnerSliderMove(event));
